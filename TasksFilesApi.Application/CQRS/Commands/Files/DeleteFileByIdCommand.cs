8646eac73c671e4c8718c4d5836f7cf6949f60a4
nickson444@mail.ru <nickson444@mail.ru>
2022-03-02 14:49:24 +0300

    file delete

--- TasksFilesApi.Application/CQRS/Commands/Files/DeleteFileByIdCommand.cs
+++ TasksFilesApi.Application/CQRS/Commands/Files/DeleteFileByIdCommand.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using TasksFilesApi.Services.Interfaces;
 
 namespace TasksFilesApi.Application.CQRS.Commands.Files
 {
@@ -18,10 +19,12 @@ namespace TasksFilesApi.Application.CQRS.Commands.Files
     public class DeleteFileByIdCommandHandler : IRequestHandler<DeleteFileByIdCommand, bool>
     {
         private readonly IMainContext _context;
+        private readonly IStorageService _storage;
 
-        public DeleteFileByIdCommandHandler(IMainContext context)
+        public DeleteFileByIdCommandHandler(IMainContext context, IStorageService storage)
         {
             _context = context;
+            _storage = storage;
         }
 
         public async Task<bool> Handle(DeleteFileByIdCommand command, CancellationToken cancellationToken)
@@ -30,6 +33,7 @@ namespace TasksFilesApi.Application.CQRS.Commands.Files
             if (file == null) 
                 return false;
 
+            _storage.Delete(file.ExtGuid);
             _context.Files.Remove(file);
 
             await _context.SaveChangesAsync();
